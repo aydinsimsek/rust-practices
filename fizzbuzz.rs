e1b1a9593522374742e15d9bfde3a78e9b6467bc
Aydin Simsek <43919074+aydinsimsek@users.noreply.github.com>
2020-11-17 00:18:28 +0300

    the classic FizzBuzz problem

---
+++ fizzbuzz.rs
@@ -0,0 +1,13 @@
+fn main()
+{
+    let mut num = 1; 
+
+    while num <= 100 
+    {
+        if num % 15 == 0 {println!("FizzBuzz");}
+        else if num % 3 == 0 {println!("Fizz");} 
+        else if num % 5 == 0 {println!("Buzz");} 
+        else {println!("{}", num);} 
+        num += 1; 
+    }
+}
